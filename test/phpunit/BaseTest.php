24d8083b4dcbb18c241484fb509923993ab72681
David McCart <davethemac@hotmail.co.uk>
2017-06-13 22:05:42 +0100

    add minimal phpunit tests just to show its working

---
+++ test/phpunit/BaseTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of BaseTest
+ *
+ * @author davethemac
+ */
+class BaseTest extends TestCase {
+    //put your code here
+    public function testPHPUnit() {
+        //var_dump($this);
+        $this->assertTrue(true);
+    }
+}
